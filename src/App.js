079d3258b09e7db2df6131102c76e88cf5c25314
Tammy Tian <tiantammy@outlook.com>
2018-06-27 21:30:44 +0200

    added search

--- src/App.js
+++ src/App.js
@@ -17,47 +17,18 @@ import './App.css';
 //onClick => run the binary search on the dataset, toggle = 1
 
 
-export default class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      searchTerm:'',
-      searchToggle: ''
-      //0 = linear, 1 = binary
-    }
-    //this.onLinearClick = this.onLinarClick(this);
-  }
-
-  onLinearClick = (e) =>{
-    e.preventDefault();
-    console.log('linear button clicked')
-    this.setState({
-      searchToggle: 'linear'
-    });
-    console.log(this.state.searchToggle)
-  }
-
-  onBinaryClick = (e) => {
-    e.preventDefault();
-    console.log('binary button clicked')
-    this.setState({
-      searchToggle: 'binary'
-    });
-    console.log(this.state.searchToggle)
-  }
-
-  //data = this.props.data
+export default class App extends React.Component {
+  
+  data = this.props.data
+  
   render() {
+    console.log(this.data)
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Search two ways</h1>
         </header>
-        <SearchForm 
-          onChange={(value) => this.setState({searchTerm: value})} 
-          onClick={(e) => this.onLinearClick(e)} 
-          onBinaryClick={(e) => this.onBinaryClick(e)}
-        />
+        <SearchForm data={this.data}/>
         <p>This is where the results will go</p>
       </div>
     );
--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,106 @@
 import React from 'react';
 
-export default function SearchForm(props){
-  return(
-    <form>
-      <label>Search:
-      <input
-        type='search'
-        placeholder='write a number'
-        
-      />
-      </label>
-      <button className='linear' 
-      onClick={(e)=> props.onClick(e)}>
-      Linear</button>
-      <button className='binary' 
-      onClick={(e) => props.onBinaryClick(e)}>
-      Binary</button>
-    </form>
-  )
+
+export default class SearchForm extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      data: this.props.data,
+      value: null,
+      counter: 0,
+    }
+  }
+  
+  handleChange(e){
+    this.setState({value: e.target.value})
+    console.log('handle search ran')
+    console.log(this.state.value)
+
+  }
+
+  onLinearClick = (e) =>{
+    e.preventDefault();
+    console.log('linear button clicked')
+    this.linearSearch(parseInt(this.state.value, 10));
+  }
+
+  linearSearch = (num) => {
+  //[89, 30, 25, 32, 72]
+    console.log('this.linearSearch ran')
+    console.log('this is value inserted to function', num);
+    let data = this.state.data;
+    console.log('this is data', data)
+    //let counter = this.state.counter;
+    for(let i=0; i<data.length; i++){
+      //console.log(data[i]);
+      //let currentCount = this.state.counter;
+      //this.setState({counter: currentCount++});
+      if(data[i] === num){
+          console.log(data[i]);
+         return data[i]
+      } 
+      //console.log(this.state.counter);
+    }
+    console.log('Value not found');
+  }
+
+  onBinaryClick = (e) => {
+    e.preventDefault();
+    console.log('binary button clicked')
+    let data = this.state.data; 
+    let value =  parseInt(this.state.value, 10);
+    this.binarySearch(data, value);
+  }
+
+  binarySearch = (array, value, start, end) => {
+    array = array.sort();
+    start = (start === undefined) ? 0 : start
+    end = (end === undefined) ? array.length : end;
+
+    if (start > end) {
+        return -1;
+    }
+
+    const index = Math.floor((start + end) / 2);
+    const item = array[index];
+
+    // console.log(start, end);
+    // console.log('this is item', item)
+    // console.log('this is index', index)
+    //[89, 30, 25, 32, 72]
+    if (item === value) {
+        return index;
+    }
+    else if (item < value) {
+        return this.binarySearch(array, value, index + 1, end);
+    }
+    else if (item > value) {
+        return this.binarySearch(array, value, start, index - 1);
+    }
+
+    return 'value not found'
+};
+  render() {
+    //console.log('this is data', this.state.data)
+    console.log('this is state', this.state)
+
+    return(
+      <form>
+        <label>Search:
+        <input
+          type='search'
+          placeholder='write a number'
+          onChange={(e) => this.handleChange(e)}
+          //value={this.state.value} 
+        />
+        </label>
+        <button type='submit' className='linear' 
+        onClick={(e)=> this.onLinearClick(e)}>
+        Linear</button>
+        <button type='submit' className='binary' 
+        onClick={(e) => this.onBinaryClick(e)}>
+        Binary</button>
+      </form>
+    )
+  }
 }
